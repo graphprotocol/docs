0fd1c2b41be387d5168a381f9209d7dc445be4a3
Benoît Rouleau <benoit.rouleau@icloud.com>
2022-05-03 12:56:59 -0400

    Make the current page’s “group” (if there is one) accessible to Algolia’s Crawler (#118)

--- layout/NavContext.ts
+++ layout/NavContext.ts
@@ -1,14 +1,14 @@
 import { createContext, Context } from 'react'
 
-import { NavItem, NavItemPage } from '@/navigation'
+import { NavItem, NavItemPage, NavItemGroup } from '@/navigation'
 
 export type NavContextProps = {
   pagePath: string
   navItems: NavItem[]
-  pageNavItems: NavItemPage[]
   previousPage: NavItemPage | null
   currentPage: NavItemPage | null
   nextPage: NavItemPage | null
+  currentGroup: NavItemGroup | null
 }
 
 export const NavContext = createContext(null) as Context<NavContextProps | null>
