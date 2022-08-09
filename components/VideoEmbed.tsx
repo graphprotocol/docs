453b431517b191f882c9980be8310987119b6a07
Benoît Rouleau <benoit.rouleau@icloud.com>
2022-08-09 13:12:23 -0400

    Add `youtube` prop to `VideoEmbed` component

--- components/VideoEmbed.tsx
+++ components/VideoEmbed.tsx
@@ -1,11 +1,27 @@
 import { HTMLAttributes } from 'react'
 
-export type VideoProps = { src: string } & HTMLAttributes<HTMLElement>
+export type VideoProps = (
+  | {
+      /** Video embed URL. */
+      src: string
+      youtube?: never
+    }
+  | {
+      /** YouTube video ID. */
+      youtube: string
+      src?: never
+    }
+) &
+  HTMLAttributes<HTMLElement>
 
-export const VideoEmbed = ({ src, ...props }: VideoProps) => {
+export const VideoEmbed = ({ src, youtube, ...props }: VideoProps) => {
   return (
     <figure sx={{ paddingBottom: `${100 / (16 / 9)}%` }} {...props}>
-      <iframe src={src} allowFullScreen sx={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }} />
+      <iframe
+        src={src ?? `https://www.youtube.com/embed/${youtube}`}
+        allowFullScreen
+        sx={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }}
+      />
     </figure>
   )
 }
