bf241f61ad3c6492c1b963ef1e84da6795b32b5a
Micmac.eth <56690114+MichaelMacaulay@users.noreply.github.com>
2022-12-22 13:35:37 -0500

    Tokenomics

--- navigation/navigation.ts
+++ navigation/navigation.ts
@@ -46,6 +46,9 @@ export const navigation = (locale: AppLocale): NavItemDefinition[] => [
     slug: 'network-transition-faq',
   },
   {
+    slug: 'tokenomics',
+  },
+  {
     divider: true,
   },
   {
