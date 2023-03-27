2c35d786f7506e611640de83e9cd15a2993aa8f3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-03-27 20:35:06 +0200

    fixes

--- .remarkrc.cjs
+++ .remarkrc.cjs
@@ -1,18 +1,7 @@
 module.exports = {
   plugins: [
-    // "preset-lint-consistent", // Check that markdown is consistent.
-    // "preset-lint-recommended", // Few recommended rules.
-    // "preset-lint-markdown-style-guide", // Markdown style guide.
-    // "preset-prettier",
-    // `remark-lint-list-item-indent` is configured with `tab-size` in the
-    // recommended preset, but if we’d prefer something else, it can be
-    // reconfigured:
-    // [
-    //   "remark-lint-list-item-indent",
-    //   "space"
-    // ]
-    'frontmatter',
+    'frontmatter', // should be defined
     ['remark-lint-first-heading-level', 2],
-    ['remark-lint-no-multiple-toplevel-headings', 0]
+    ['remark-lint-restrict-elements', { type: 'heading', depth: 1 }],
   ],
 }
