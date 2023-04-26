726a4b747656367ded74b4907b73f27d3228bc67
Dimitri POSTOLOV <en3m@ya.ru>
2023-04-26 15:28:21 +0200

    Forbid h1 `# ...` elements, enforce h2 `## ...` as first heading on page (#334)

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ['*.{ts,tsx,js,jsx,mjs,cjs}'],
+      extends: ['@edgeandnode/eslint-config', '@edgeandnode/eslint-config/next'],
+      settings: {
+        next: { rootDir: 'website' },
+      },
+    },
+    {
+      files: ['*.{ts,tsx}'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+    {
+      // We lint only english pages because other languages will be translated from english
+      files: ['website/pages/en/**/*.{md,mdx}'],
+      parser: 'eslint-mdx',
+      processor: 'mdx/remark',
+      plugins: ['mdx'],
+      rules: {
+        'mdx/remark': 'error',
+      },
+    },
+  ],
+}
