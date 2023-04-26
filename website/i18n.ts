2567953d9ec1750e04ed3a3d1beda7d7ea985355
Benoît Rouleau <benoit.rouleau@icloud.com>
2023-04-26 15:42:20 -0400

    Enable 2 new languages: Hindi and Urdu

--- website/i18n.ts
+++ website/i18n.ts
@@ -3,8 +3,10 @@ import { Locale, NestedStrings, Translations, useI18n as _useI18n } from '@edgea
 import ar from '@/pages/ar/translations'
 import en from '@/pages/en/translations'
 import es from '@/pages/es/translations'
+import hi from '@/pages/hi/translations'
 import ja from '@/pages/ja/translations'
 import ko from '@/pages/ko/translations'
+import ur from '@/pages/ur/translations'
 import vi from '@/pages/vi/translations'
 import zh from '@/pages/zh/translations'
 
@@ -12,8 +14,10 @@ const appLocales = [
   Locale.ARABIC,
   Locale.ENGLISH,
   Locale.SPANISH,
+  Locale.HINDI,
   Locale.JAPANESE,
   Locale.KOREAN,
+  Locale.URDU,
   Locale.VIETNAMESE,
   Locale.CHINESE,
 ] as const
@@ -37,8 +41,10 @@ export const translations = {
   ar,
   en,
   es,
+  hi,
   ja,
   ko,
+  ur,
   vi,
   zh,
 }
---
+++ website/pages/pt/translations.ts
@@ -0,0 +1,11 @@
+import docsearch from './docsearch.json'
+import global from './global.json'
+import index from './index.json'
+
+const translations = {
+  global,
+  index,
+  docsearch,
+}
+
+export default translations
