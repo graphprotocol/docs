382dfa0a835054f770804d89bfefe8f3693bb770
Benoît Rouleau <benoit.rouleau@icloud.com>
2023-06-07 18:10:04 -0400

    Turn Supported Networks into a table

--- website/src/getSupportedNetworks.ts
+++ website/src/getSupportedNetworks.ts
@@ -1,26 +1,10 @@
 import { ChainProductStatus, SupportedNetworkMap } from '@edgeandnode/common'
 
-const categories = ['studio', 'hostedService'] as const
-
-type Category = (typeof categories)[number]
-
 const isNotNull = <T>(x: T | null): x is T => x !== null
 
-export function getSupportedNetworks(category: Category) {
+export function getSupportedNetworks() {
   return Array.from(SupportedNetworkMap.values()).flatMap((network) =>
     Array.from(network.chains)
-      .filter((chain) => {
-        // Don't return chains that are not supported in this category
-        if (chain.productDeployStatus[category] !== ChainProductStatus.ALLOWED) {
-          return false
-        }
-        // Don't repeat the chains that are supported in Studio in the Hosted Service category
-        // eslint-disable-next-line sonarjs/prefer-single-boolean-return
-        if (category === 'hostedService' && chain.productDeployStatus['studio'] === ChainProductStatus.ALLOWED) {
-          return false
-        }
-        return true
-      })
       .map((chain) => {
         if (!chain.graphCliName) {
           return null
@@ -28,7 +12,11 @@ export function getSupportedNetworks(category: Category) {
           return {
             name: chain.name,
             cliName: chain.graphCliName,
-            beta: chain.graphCliName !== 'mainnet', // TODO: Include a `beta` property in `@edgeandnode/common`?
+            chainId: chain.chainId,
+            supportedOnHosted: chain.productDeployStatus.hostedService === ChainProductStatus.ALLOWED,
+            supportedOnStudio: chain.studioHosted || chain.productDeployStatus.studio === ChainProductStatus.ALLOWED,
+            supportedOnNetwork: chain.networkPublishChainAllowStatusMap != null,
+            isBeta: chain.graphCliName !== 'mainnet', // TODO: Include a `beta` property in `@edgeandnode/common`?
           }
         }
       })
