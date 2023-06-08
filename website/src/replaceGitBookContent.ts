7b368afcb7a1d016915e89361f87a43813a406b4
Dimitri POSTOLOV <en3m@ya.ru>
2023-06-08 20:35:06 +0200

    Update website/src/replaceGitBookContent.ts
    Co-authored-by: Benoît Rouleau <benoit.rouleau@icloud.com>

--- website/src/replaceGitBookContent.ts
+++ website/src/replaceGitBookContent.ts
@@ -17,7 +17,7 @@ export function replaceGitBookContent({ content, user, repo, branch, docsPath }:
             // we need to enhance YouTube links, otherwise they will be not loaded in iframe
             'youtube.com/watch?v=',
             'youtube.com/embed/'
-          )}" style={{aspectRatio: 16/9, width: '100%'}}/>`
+          )}" style={{ aspectRatio: 16/9, width: '100%' }} />`
       )
       // remove gitbook {% ... %} elements
       .replaceAll(/{%.*?%}/g, '')
