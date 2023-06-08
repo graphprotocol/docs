842b184ffc177c63ce6f96a315232456cbc37195
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-08 20:31:39 +0200

    add Graph Client README.md + fix network diagram for substreams/README

--- website/pages/en/querying/graph-client/_meta.js
+++ website/pages/en/querying/graph-client/_meta.js
@@ -2,4 +2,7 @@ import { createCatchAllMeta } from 'nextra/catch-all'
 
 import json from '../../../../remote-files/graph-client.json' assert { type: 'json' }
 
-export default () => createCatchAllMeta(json.filePaths)
+export default () =>
+  createCatchAllMeta(json.filePaths, {
+    README: 'Introduction',
+  })
---
+++ website/src/replaceGitBookContent.ts
@@ -0,0 +1,33 @@
+type ReplaceGitBookContentParams = {
+  content: string
+  user: string
+  repo: string
+  branch: string
+  docsPath: string
+}
+
+export function replaceGitBookContent({ content, user, repo, branch, docsPath }: ReplaceGitBookContentParams): string {
+  return (
+    content
+      // replace {% embed ... %} with <iframe />
+      .replaceAll(
+        /{%\s+embed\s+url="(.*?)"\s+%}/g,
+        (...m) =>
+          `<iframe src="${m[1].replace(
+            // we need enhance YouTube links, otherwise they will be not loaded in iframe
+            'youtube.com/watch?v=',
+            'youtube.com/embed/'
+          )}" style={{aspectRatio: 16/9, width: '100%'}}/>`
+      )
+      // remove gitbook {% ... %} elements
+      .replaceAll(/{%.*?%}/g, '')
+      // close unclosed img tags
+      .replaceAll(/<img((?:(?!\/>)[^>])*?)>/g, (...m) => `<img${m[1]}/>`)
+      // Replaces all the relative paths of images to absolute paths to the repo
+      .replaceAll('../assets', `https://raw.githubusercontent.com/${user}/${repo}/${branch}/assets/`)
+      .replaceAll(
+        '.gitbook/assets/',
+        `https://raw.githubusercontent.com/${user}/${repo}/${branch}/${docsPath}.gitbook/assets/`
+      )
+  )
+}
