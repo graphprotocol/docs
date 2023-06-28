02d01369eda33dfac5ca98fac577d58d6d24fedb
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-06-28 13:45:31 +0200

    move tests

--- packages/og-image/__tests__/handler.test.ts
+++ packages/og-image/__tests__/handler.test.ts
@@ -1,33 +1,33 @@
-import { handler } from './handler';
+import { handler } from '../src/handler'
 
 vi.mock('../vender/index_bg.wasm', async () => {
-  const fs = await import('node:fs/promises');
-  const wasm = await fs.readFile(require.resolve('@resvg/resvg-wasm/index_bg.wasm'));
+  const fs = await import('node:fs/promises')
+  const wasm = await fs.readFile(require.resolve('@resvg/resvg-wasm/index_bg.wasm'))
   return {
     default: wasm,
-  };
-});
+  }
+})
 
 describe('handler()', () => {
   it('should works', async () => {
     const response = await handler({
       url: 'http://localhost:3000',
-    });
-    const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
-  });
+    } as Request)
+    const result = Buffer.from(await response.arrayBuffer())
+    expect(result).toMatchImageSnapshot()
+  })
   it('should align title and have container padding', async () => {
     const response = await handler({
       url: 'http://localhost:3000?title=Hello this is a test of really really really really really really long title',
-    });
-    const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
-  });
+    } as Request)
+    const result = Buffer.from(await response.arrayBuffer())
+    expect(result).toMatchImageSnapshot()
+  })
   it('should align title without whitespaces', async () => {
     const response = await handler({
       url: 'http://localhost:3000?title=Home',
-    });
-    const result = Buffer.from(await response.arrayBuffer());
-    expect(result).toMatchImageSnapshot();
-  });
-});
+    } as Request)
+    const result = Buffer.from(await response.arrayBuffer())
+    expect(result).toMatchImageSnapshot()
+  })
+})
--- packages/og-image/src/index.ts
+++ packages/og-image/src/index.ts
@@ -27,12 +27,12 @@ export default {
       // If not in cache, get it from origin
       response = await handler(request);
 
-      if (process.env.NODE_ENV !== 'test' && ![404, 500].includes(response.status)) {
-        // Any changes made to the response here will be reflected in the cached value
-        response.headers.append('Cache-Control', 'public');
-        response.headers.append('Cache-Control', `s-maxage=${maxAgeForCDN}`);
-        response.headers.append('Cache-Control', `max-age=${maxAgeForBrowser}`);
-      }
+      // if (process.env.NODE_ENV !== 'test' && ![404, 500].includes(response.status)) {
+      //   // Any changes made to the response here will be reflected in the cached value
+      //   response.headers.append('Cache-Control', 'public');
+      //   response.headers.append('Cache-Control', `s-maxage=${maxAgeForCDN}`);
+      //   response.headers.append('Cache-Control', `max-age=${maxAgeForBrowser}`);
+      // }
 
       // Store the fetched response as cacheKey
       // Use `waitUntil`, so you can return the response without blocking on
