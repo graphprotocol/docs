966d73667f5123a270d40fba02a6f4ba5bbb9e59
Benoît Rouleau <benoit.rouleau@icloud.com>
2023-11-18 18:12:16 -0500

    Try to fix the build

--- website/next.config.js
+++ website/next.config.js
@@ -39,6 +39,7 @@ export const getStaticProps = async context => ({
 export default withNextra({
   env,
   output: 'export',
+  distDir: '../out/docs',
   experimental: {
     // Fix scroll restoration (see https://github.com/vercel/next.js/issues/37893#issuecomment-1221335543)
     scrollRestoration: true,
