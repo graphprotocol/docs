ef00f612002d188784c47943f5918b720e5c6d94
Benoît Rouleau <benoit.rouleau@icloud.com>
2024-02-08 14:06:22 -0500

    Update Supported Networks on Get Started page + visual improvements to search

--- packages/nextra-theme/src/components/DocSearch.tsx
+++ packages/nextra-theme/src/components/DocSearch.tsx
@@ -1,4 +1,5 @@
 import { DocSearchModal, DocSearchProps, useDocSearchKeyboardEvents } from '@docsearch/react'
+import { keyframes } from '@emotion/react'
 import { useCallback, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { Global } from 'theme-ui'
@@ -12,6 +13,7 @@ import {
   FontSize,
   FontWeight,
   Icon,
+  LineHeight,
   Link,
   Opacity,
   Responsive,
@@ -19,6 +21,11 @@ import {
   Text,
   useTheme,
 } from '@edgeandnode/gds'
+
+const animationFade = keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+})
 
 export function DocSearch(props: DocSearchProps) {
   const searchButtonRef = useRef<HTMLButtonElement>(null)
@@ -153,9 +160,9 @@ export function DocSearch(props: DocSearchProps) {
                 '--docsearch-spacing': '0 !important',
                 '--docsearch-container-background': `${theme.colors!.Midnight88} !important`,
                 '--docsearch-modal-width': 'min(calc(100vw - 32px), 840px) !important',
-                '--docsearch-modal-background': '#292738 !important',
+                '--docsearch-modal-background': `${theme.colors!.Tooltip} !important`,
                 '--docsearch-modal-shadow': 'none !important',
-                '--docsearch-searchbox-height': '96px !important',
+                '--docsearch-searchbox-height': ['64px !important', null, '96px !important'],
                 '--docsearch-searchbox-focus-background': 'transparent !important',
                 '--docsearch-searchbox-shadow': 'none !important',
                 '--docsearch-hit-background': 'transparent !important',
@@ -163,20 +170,23 @@ export function DocSearch(props: DocSearchProps) {
                 '--docsearch-hit-active-color': `${theme.colors!.White} !important`,
                 '--docsearch-highlight-color': `${theme.colors!.Purple} !important`,
               },
+              '.DocSearch-Container': {
+                animation: `${animationFade} 150ms ease-in-out`,
+              },
               '.DocSearch-Modal': {
                 m: [null, null, '96px auto auto'],
-                border: buildBorder('White8'),
+                border: [null, null, 'White8'],
                 overflow: 'hidden',
                 cursor: 'default',
               },
               '.DocSearch-SearchBar': {
-                p: `0 ${Spacing['24px']}`,
+                p: [`0 ${Spacing['16px']}`, null, `0 ${Spacing['24px']}`],
                 '&::after': {
                   content: `''`,
                   zIndex: 100,
                   position: 'absolute',
-                  left: Spacing['24px'],
-                  right: Spacing['24px'],
+                  left: [Spacing['16px'], null, Spacing['24px']],
+                  right: [Spacing['16px'], null, Spacing['24px']],
                   bottom: '-1px',
                   borderBottom: buildBorder('White16'),
                   bg: 'var(--docsearch-modal-background)',
@@ -188,9 +198,14 @@ export function DocSearch(props: DocSearchProps) {
               '.DocSearch-MagnifierLabel, .DocSearch-LoadingIndicator': {
                 color: 'white',
                 svg: {
-                  width: '18px',
-                  height: '18px',
+                  width: '20px',
+                  height: '20px',
                 },
+              },
+              '.DocSearch-Search-Icon': {
+                left: '3px',
+                color: 'transparent',
+                backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" style="fill: white;"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.70768 1C9.85995 1 12.4154 3.55542 12.4154 6.70768C12.4154 8.08963 11.9242 9.35686 11.107 10.3444C11.3349 10.5734 11.6474 10.8859 12.0424 11.2809L12.4833 11.7218C13.1109 12.3494 13.8972 13.1357 14.8423 14.0808C15.0526 14.2911 15.0526 14.632 14.8423 14.8423C14.632 15.0526 14.2911 15.0526 14.0808 14.8423L10.3444 11.107C9.35686 11.9242 8.08963 12.4154 6.70768 12.4154C3.55542 12.4154 1 9.85995 1 6.70768C1 3.55542 3.55542 1 6.70768 1ZM6.70768 2.07692C4.15018 2.07692 2.07692 4.15018 2.07692 6.70768C2.07692 9.26518 4.15018 11.3384 6.70768 11.3384C9.26518 11.3384 11.3384 9.26518 11.3384 6.70768C11.3384 4.15018 9.26518 2.07692 6.70768 2.07692Z" /></svg>')`,
               },
               '.DocSearch-Input': {
                 p: `${Spacing['32px']} ${Spacing['16px']}`,
@@ -232,9 +247,10 @@ export function DocSearch(props: DocSearchProps) {
               '.DocSearch-Hit-source': {
                 m: 0,
                 mb: Spacing['2px'],
-                p: `0 ${Spacing['16px']}`,
+                p: `${Spacing['8px']} ${Spacing['16px']}`,
                 fontWeight: FontWeight.MEDIUM,
                 fontSize: FontSize['12px'],
+                lineHeight: LineHeight.TIGHT,
                 textTransform: 'uppercase',
                 letterSpacing: '0.15em',
                 color: 'White32',
@@ -327,8 +343,19 @@ export function DocSearch(props: DocSearchProps) {
               },
               '.DocSearch-NoResults-Prefill-List': {
                 p: 0,
-                mt: Spacing['32px'],
+                mt: Spacing['48px'],
                 textAlign: 'center',
+                ul: {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: Spacing['8px'],
+                  li: {
+                    listStyle: 'none',
+                  },
+                },
+              },
+              '.DocSearch-Prefill': {
+                fontWeight: FontWeight.REGULAR,
               },
             }}
           />
