58d96fc145d7844a745ee94b1a156f1ca21c06ab
Benoît Rouleau <benoit.rouleau@icloud.com>
2024-04-09 14:50:30 -0400

    Tweaks

--- website/src/contracts.tsx
+++ website/src/contracts.tsx
@@ -1,31 +1,30 @@
 import ContractAddresses from '@graphprotocol/contracts/addresses.json'
 import { Table } from '@graphprotocol/nextra-theme'
 
-export function ProtocolContractsTable({
-  networkId,
-  blockExplorer,
-}: {
-  networkId: keyof typeof ContractAddresses
-  blockExplorer: string
-}) {
-  const contracts = ContractAddresses[networkId]
+import { getAddressLink } from '@edgeandnode/common'
+import { Link } from '@edgeandnode/gds'
+
+type ValueOf<T> = T[keyof T]
+const contractsByNetworkId = ContractAddresses as Record<string, ValueOf<typeof ContractAddresses>>
+
+export function ProtocolContractsTable({ networkId }: { networkId: number }) {
+  const contracts = Object.entries(contractsByNetworkId[`${networkId}`] ?? {}).map(([name, contract]) => ({
+    ...contract,
+    name,
+  }))
   return (
     <Table>
       <tbody>
         <tr>
           <th>{'Contract'}</th>
-          <th align="center">{'Address'}</th>
+          <th>{'Address'}</th>
         </tr>
-        {Object.entries(contracts).map(([contract, info]) => {
+        {contracts.map((contract) => {
           return (
-            <tr key={contract}>
-              <td>{contract}</td>
-              <td align="center">
-                {
-                  <a href={`https://${blockExplorer}/address/${info.address}`} target="_blank" rel="noreferrer">
-                    {info.address}
-                  </a>
-                }
+            <tr key={contract.name}>
+              <td>{contract.name}</td>
+              <td>
+                <Link href={getAddressLink(contract.address, networkId)}>{contract.address}</Link>
               </td>
             </tr>
           )
