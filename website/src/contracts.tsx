fec71c9ecdfcf2874018e64f8de3a449398c875d
azf20 <azfuller20@gmail.com>
2024-04-09 18:55:16 +0100

    linting

--- website/src/contracts.tsx
+++ website/src/contracts.tsx
@@ -1,8 +1,13 @@
-import { Table } from '@graphprotocol/nextra-theme'
 import ContractAddresses from '@graphprotocol/contracts/addresses.json'
+import { Table } from '@graphprotocol/nextra-theme'
 
-export function ProtocolContractsTable({networkId, blockExplorer}: {networkId: keyof typeof ContractAddresses, blockExplorer: string}) {
-
+export function ProtocolContractsTable({
+  networkId,
+  blockExplorer,
+}: {
+  networkId: keyof typeof ContractAddresses
+  blockExplorer: string
+}) {
   const contracts = ContractAddresses[networkId]
   return (
     <Table>
@@ -12,10 +17,18 @@ export function ProtocolContractsTable({networkId, blockExplorer}: {networkId: k
           <th align="center">{'Address'}</th>
         </tr>
         {Object.entries(contracts).map(([contract, info]) => {
-          return (<tr key={contract}>
-            <td>{contract}</td>
-            <td align="center">{<a href={`https://${blockExplorer}/address/${info.address}`} target='_blank' >{info.address}</a>}</td>
-          </tr>)
+          return (
+            <tr key={contract}>
+              <td>{contract}</td>
+              <td align="center">
+                {
+                  <a href={`https://${blockExplorer}/address/${info.address}`} target="_blank" rel="noreferrer">
+                    {info.address}
+                  </a>
+                }
+              </td>
+            </tr>
+          )
         })}
       </tbody>
     </Table>
