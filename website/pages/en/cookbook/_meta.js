3a326e0825c547303f7471c70e5cfdb5c08570ea
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2024-05-24 11:08:52 -0400

    Improved cookbook formatting and re-ordered cookbooks

--- website/pages/en/cookbook/_meta.js
+++ website/pages/en/cookbook/_meta.js
@@ -1,6 +1,10 @@
 export default {
-  'base-testnet': '',
   'upgrading-a-subgraph': '',
+  'subgraph-best-practice-1': 'Subgraph Best Practice 1: Pruning with indexerHints',
+  'subgraph-best-practice-2': 'Subgraph Best Practice 2: Manage Arrays with @derivedFrom',
+  'subgraph-best-practice-3': 'Subgraph Best Practice 3: Using Immutable Entities and Bytes as IDs',
+  'subgraph-best-practice-4': 'Subgraph Best Practice 4: Avoid eth_calls',
+  'base-testnet': '',
   'subgraph-debug-forking': '',
   near: '',
   cosmos: '',
@@ -8,8 +12,4 @@ export default {
   grafting: '',
   'subgraph-uncrashable': '',
   'substreams-powered-subgraphs': '',
-  'subgraph-best-practice-1': 'Subgraph Best Practice 1: Pruning with indexerHints',
-  'subgraph-best-practice-2': 'Subgraph Best Practice 2: Manage Arrays with @derivedFrom',
-  'subgraph-best-practice-3': 'Subgraph Best Practice 3: Using Immutable Entities and Bytes as IDs',
-  'subgraph-best-practice-4': 'Subgraph Best Practice 4: Avoid eth_calls'
 }
