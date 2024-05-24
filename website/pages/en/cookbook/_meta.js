31ba40f7eebc6754dd6931da5d991f372ba5a6d1
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2024-05-24 13:18:22 -0400

    Changed file naming to be SEO optimized

--- website/pages/en/cookbook/_meta.js
+++ website/pages/en/cookbook/_meta.js
@@ -8,8 +8,8 @@ export default {
   grafting: '',
   'subgraph-uncrashable': '',
   'substreams-powered-subgraphs': '',
-  'subgraph-best-practice-1': 'Subgraph Best Practice 1: Pruning with indexerHints',
-  'subgraph-best-practice-2': 'Subgraph Best Practice 2: Manage Arrays with @derivedFrom',
-  'subgraph-best-practice-3': 'Subgraph Best Practice 3: Using Immutable Entities and Bytes as IDs',
-  'subgraph-best-practice-4': 'Subgraph Best Practice 4: Avoid eth_calls',
+  pruning: 'Subgraph Best Practice 1: Pruning with indexerHints',
+  derivedFrom: 'Subgraph Best Practice 2: Manage Arrays with @derivedFrom',
+  'immutable-entities-bytes-as-ids': 'Subgraph Best Practice 3: Using Immutable Entities and Bytes as IDs',
+  'avoid-eth-calls': 'Subgraph Best Practice 4: Avoid eth_calls',
 }
