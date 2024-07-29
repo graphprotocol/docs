7d7b75d486654427c5da8411f5a458bf819b1bd3
Anton Dosta <a.dosta@icloud.com>
2024-07-29 03:48:11 -0700

    feat: Cookbook Onboard integration

--- website/src/env.d.ts
+++ website/src/env.d.ts
@@ -2,3 +2,8 @@ declare namespace globalThis {
   import { Locale } from '@edgeandnode/gds'
   var __graph_docs_locale: Locale
 }
+
+// Solves the following error: "Cannot find module ... or its corresponding type declarations. There are types at ..., but this result could not be resolved under your current 'moduleResolution' setting. Consider updating to 'node16', 'nodenext', or 'bundler'. [2307]"
+declare module "@cookbookdev/docsbot/react-fixed" {
+  export { default } from "@cookbookdev/docsbot/dist/react-fixed/index.d.ts"
+}
