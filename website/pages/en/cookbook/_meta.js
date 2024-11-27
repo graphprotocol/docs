0a4879565f246f063f4920d8d1c24d943e23a73f
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2024-11-27 13:02:43 -0500

    updated subgraph best practices
    ... and added links to bottom of each best practice

--- website/pages/en/cookbook/_meta.js
+++ website/pages/en/cookbook/_meta.js
@@ -11,6 +11,7 @@ export default {
   derivedfrom: 'Subgraph Best Practice 2: Manage Arrays with @derivedFrom',
   'immutable-entities-bytes-as-ids': 'Subgraph Best Practice 3: Using Immutable Entities and Bytes as IDs',
   'avoid-eth-calls': 'Subgraph Best Practice 4: Avoid eth_calls',
-  'timeseries': 'Subgraph Best Practice 5: Simplify and Optimize with Timeseries and Aggregationsnd Aggregations',
+  timeseries: 'Subgraph Best Practice 5: Simplify and Optimize with Timeseries and Aggregations',
+  'grafting-hotfix': 'Subgraph Best Practice 6: Grafting and Hotfixing',
   enums: '',
 }
