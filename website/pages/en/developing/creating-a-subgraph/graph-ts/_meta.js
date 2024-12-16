56af1cbfa9f6ebf8a08dfa50408d92d162560b44
benface <benoit.rouleau@icloud.com>
2024-12-16 14:05:42 -0500

    Fix import

--- website/pages/en/developing/creating-a-subgraph/graph-ts/_meta.js
+++ website/pages/en/developing/creating-a-subgraph/graph-ts/_meta.js
@@ -1,6 +1,6 @@
 import { createCatchAllMeta } from 'nextra/catch-all'
 
-import json from '../../../../remote-files/graph-ts.json' assert { type: 'json' }
+import json from '../../../../../remote-files/graph-ts.json' assert { type: 'json' }
 
 export default () =>
   createCatchAllMeta(json.filePaths, {
