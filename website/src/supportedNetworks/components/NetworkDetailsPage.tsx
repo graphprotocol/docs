481de29fb0f15b4159d92922c660531a31c13afb
Hayder <hayder@hey.com>
2025-04-09 08:56:41 +0200

    Add resources for chain-specific landing pages (#923)
    * Add resources for chain-specific landing pages
    
    * NetworkDetailsPage - fix lint
    
    * use css for layout controls (#924)
    
    * leverage css to control layout
    
    * replace use of as any
    
    * lint
    
    ---------
    
    Co-authored-by: AK <58132405+0xa3k5@users.noreply.github.com>

--- website/src/supportedNetworks/components/NetworkDetailsPage.tsx
+++ website/src/supportedNetworks/components/NetworkDetailsPage.tsx
@@ -1,128 +1,83 @@
-import { memo } from 'react'
-
-import { Subgraph, Substreams, SubstreamsPoweredSubgraph } from '@edgeandnode/gds/icons'
-
 import { Card, TimeIcon } from '@/components'
 import { useI18n } from '@/i18n'
 
-import { isEVMNetwork, type Network } from '../utils'
+import { isEVMNetwork, type Network, type NetworkData, type ProcessedNetwork, supportsTokenAPI } from '../utils'
+
+import {
+  evmNoTokenAPICards,
+  evmSubgraphsOnlyCards,
+  evmWithTokenAPICards,
+  nonEvmNoTokenAPICards,
+  nonEvmWithTokenAPICards,
+} from './ResourceCards'
 
 type NetworkDetailsPageProps = {
-  network: Network
+  network: Network | ProcessedNetwork | NetworkData
 }
 
-const EVMResources = memo(() => {
+interface NetworkFeatures {
+  supportsSubgraphs: boolean
+  supportsSubstreams: boolean
+  supportsTokenAPI: boolean
+}
+
+function getNetworkFeatures(network: Network | ProcessedNetwork | NetworkData): NetworkFeatures {
+  if ('subgraphs' in network && typeof network.subgraphs === 'boolean') {
+    const processedNetwork = network
+    return {
+      supportsSubgraphs: processedNetwork.subgraphs,
+      supportsSubstreams: processedNetwork.substreams,
+      supportsTokenAPI: processedNetwork.tokenapi,
+    }
+  }
+
+  return {
+    supportsSubgraphs: 'services' in network && Boolean(network.services.subgraphs?.length),
+    supportsSubstreams: 'services' in network && Boolean(network.services.substreams?.length),
+    supportsTokenAPI: supportsTokenAPI(network.id),
+  }
+}
+
+function getResourceCardsConfig(isEVM: boolean, features: NetworkFeatures) {
+  const { supportsSubgraphs, supportsSubstreams, supportsTokenAPI } = features
+
+  if (isEVM) {
+    if (supportsSubgraphs && !supportsSubstreams) {
+      return evmSubgraphsOnlyCards
+    }
+    if (supportsTokenAPI) {
+      return evmWithTokenAPICards
+    }
+    return evmNoTokenAPICards
+  }
+
+  return supportsTokenAPI ? nonEvmWithTokenAPICards : nonEvmNoTokenAPICards
+}
+
+function NetworkDetailsPage({ network }: NetworkDetailsPageProps) {
   const { t } = useI18n()
-
-  return (
-    <div className="graph-docs-not-markdown mt-8 grid grid-cols-1 gap-4">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <Card
-          href="https://thegraph.com/docs/en/subgraphs/quick-start/"
-          title={t('index.networkGuides.evm.subgraphQuickStart.title')}
-          description={t('index.networkGuides.evm.subgraphQuickStart.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={10} />}
-          className="min-h-64"
-          icon={<Subgraph size={6} />}
-        />
-        <Card
-          href="https://docs.substreams.dev/"
-          title={t('index.networkGuides.evm.substreams.title')}
-          description={t('index.networkGuides.evm.substreams.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={15} />}
-          className="min-h-64"
-          icon={<Substreams size={6} />}
-        />
-        <Card
-          href="https://thegraph.com/docs/en/subgraphs/best-practices/timeseries/"
-          title={t('index.networkGuides.evm.timeseries.title')}
-          description={t('index.networkGuides.evm.timeseries.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={8} />}
-          className="min-h-64"
-          icon={<Substreams size={6} />}
-        />
-      </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <Card
-          href="https://thegraph.com/docs/en/subgraphs/developing/creating/advanced/"
-          title={t('index.networkGuides.evm.advancedFeatures.title')}
-          description={t('index.networkGuides.evm.advancedFeatures.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={12} />}
-        />
-        <Card
-          href="https://thegraph.com/docs/en/subgraphs/billing/"
-          title={t('index.networkGuides.evm.billing.title')}
-          description={t('index.networkGuides.evm.billing.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={5} />}
-        />
-      </div>
-    </div>
-  )
-})
-
-const NonEVMResources = memo(() => {
-  const { t } = useI18n()
-
-  return (
-    <div className="graph-docs-not-markdown mt-8 grid grid-cols-1 gap-4">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <Card
-          href="https://docs.substreams.dev/"
-          title={t('index.networkGuides.nonEvm.officialDocs.title')}
-          description={t('index.networkGuides.nonEvm.officialDocs.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={15} />}
-          className="min-h-64"
-          icon={<Substreams size={6} />}
-        />
-        <Card
-          href="https://thegraph.com/docs/en/sps/introduction/"
-          title={t('index.networkGuides.nonEvm.spsIntro.title')}
-          description={t('index.networkGuides.nonEvm.spsIntro.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={8} />}
-          className="min-h-64"
-          icon={<SubstreamsPoweredSubgraph size={6} />}
-        />
-        <Card
-          href="https://substreams.dev/"
-          title={t('index.networkGuides.nonEvm.substreamsDev.title')}
-          description={t('index.networkGuides.nonEvm.substreamsDev.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={10} />}
-          className="min-h-64"
-          icon={<Substreams size={6} />}
-        />
-      </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <Card
-          href="https://github.com/streamingfast/substreams-starter"
-          title={t('index.networkGuides.nonEvm.substreamsStarter.title')}
-          description={t('index.networkGuides.nonEvm.substreamsStarter.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={5} />}
-        />
-        <Card
-          href="https://github.com/streamingfast/substreams"
-          title={t('index.networkGuides.nonEvm.substreamsRepo.title')}
-          description={t('index.networkGuides.nonEvm.substreamsRepo.description')}
-          slotAboveTitle={<TimeIcon variant="reading" minutes={7} />}
-        />
-      </div>
-    </div>
-  )
-})
-
-EVMResources.displayName = 'EVMResources'
-NonEVMResources.displayName = 'NonEVMResources'
-
-const NetworkDetailsPage = memo(({ network }: NetworkDetailsPageProps) => {
-  const { t } = useI18n()
+  const isEVM = isEVMNetwork(network)
+  const features = getNetworkFeatures(network)
+  const cards = getResourceCardsConfig(isEVM, features)
 
   return (
     <>
-      <h3 className="text-h18 mt-0">{t('index.supportedNetworks.guides')}</h3>
-      {isEVMNetwork(network) ? <EVMResources /> : <NonEVMResources />}
+      <h3 className="text-h18">{t('index.supportedNetworks.guides')}</h3>
+      <div className="grid grid-cols-6 gap-4">
+        {cards.map((card) => (
+          <Card
+            key={t(card.titleKey)}
+            href={card.href}
+            title={t(card.titleKey)}
+            description={t(card.descriptionKey)}
+            slotAboveTitle={<TimeIcon variant="reading" minutes={card.minutes} />}
+            className="col-span-full [&:nth-child(-n+3)]:lg:col-span-2 [&:nth-child(-n+3)]:lg:min-h-64 [&:nth-child(n+4)]:lg:col-span-3"
+            icon={card.icon}
+          />
+        ))}
+      </div>
     </>
   )
-})
-
-NetworkDetailsPage.displayName = 'NetworkDetailsPage'
+}
 
 export default NetworkDetailsPage
