dcc7259b5b6ae91cc88b55684bdc1746498cff93
benface <benoit.rouleau@icloud.com>
2025-07-10 11:57:26 -0400

    Pull latest Token API JSON + use production going forward

--- website/src/pages/mr/token-api/svm/_meta.js
+++ website/src/pages/mr/token-api/svm/_meta.js
@@ -1,8 +1,5 @@
 export default {
-  'get-balances-svm-by-address': '',
+  'get-balances-svm': '',
   'get-transfers-svm': '',
-  'get-holders-svm-by-contract': '',
-  'get-tokens-svm-by-contract': '',
-  'get-ohlc-pools-svm-by-pool': '',
   'get-swaps-svm': '',
 }
