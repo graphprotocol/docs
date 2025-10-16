a722f12e06314276a83f2817f2dcc4ffca006474
benface <benoit.rouleau@icloud.com>
2025-10-16 09:54:19 -0400

    Prettier

--- website/src/pages/cs/token-api/evm-tokens/_meta.js
+++ website/src/pages/cs/token-api/evm-tokens/_meta.js
@@ -1,8 +1,8 @@
 export default {
   'balances-historical': '',
   'balances-native': '',
-  'balances': '',
-  'holders': '',
-  'tokens': '',
-  'transfers': '',
+  balances: '',
+  holders: '',
+  tokens: '',
+  transfers: '',
 }
--- website/src/pages/tr/token-api/monitoring/_meta.js
+++ website/src/pages/tr/token-api/monitoring/_meta.js
@@ -1,5 +1,5 @@
 export default {
-  'health': '',
-  'networks': '',
-  'version': '',
+  health: '',
+  networks: '',
+  version: '',
 }
