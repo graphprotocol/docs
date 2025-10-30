7aefb456f16e9c84eb379fb8a4f097d90ac08bf2
benface <benoit.rouleau@icloud.com>
2025-10-29 22:44:21 -0400

    Run `pnpm prebuild:ci`

--- website/src/pages/ko/subgraphs/developing/creating/_meta.js
+++ website/src/pages/ko/subgraphs/developing/creating/_meta.js
@@ -6,6 +6,7 @@ export default {
   'subgraph-manifest': '',
   'ql-schema': '',
   'assemblyscript-mappings': '',
+  'graph-node-dev': '',
   advanced: '',
   'graph-ts': titles['graph-ts'] ?? '',
   'unit-testing-framework': '',
