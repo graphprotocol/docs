b57938f62c66825d4e7d89b2be6aa72c724d36cc
Brandon Kramer <brandonleekramer@gmail.com>
2026-08-05 21:33:49 -0400

    Adding Subgraph Gateway Docs v1.0

---
+++ website/src/pages/en/gateways/subgraphs/components/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  overview: '',
+  'operating-a-gateway': '',
+  'indexer-selection': 'Indexer Selection',
+  'graph-tally': '',
+  titorelli: '',
+  'gateway-in-a-box': '',
+}
---
+++ website/src/pages/en/gateways/subgraphs/consumer-side/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  overview: '',
+  'pricing-payments': '',
+  'serving-queries': '',
+  'subgraph-collections': '',
+  'publication-flows': 'Publication Flows',
+  'customer-support': '',
+  'indexing-approaches': 'Indexing Approaches',
+}
